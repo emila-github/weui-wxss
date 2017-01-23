69c4ddb46e7a2a7a839e54f330a60be191f684e5
王常键 <changjianwang@cyou-inc.com>
2017-01-23 11:40:22 +0800

    wxkey

--- src/example/index.js
+++ src/example/index.js
@@ -5,7 +5,7 @@ Page({
                 id: 'form',
                 name: 'demo',
                 open: false,
-                pages: ['flextest','tapmenu', 'forlist', 'forlistrequest', 'forlisttmpimp','vrdropdown','scrollview']
+                pages: ['flextest','tapmenu', 'forlist', 'forlistrequest', 'forlisttmpimp', 'vrlist','vrdropdown','scrollview', 'wxkey']
             },
             {
                 id: 'form',
